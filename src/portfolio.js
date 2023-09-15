54bb0a14362f929076d29f5f6c6bfeb0245bf3f0
dalibouali <boualimohamed77@gmail.com>
2023-09-15 20:54:40 +0100

    stable virsion

--- src/portfolio.js
+++ src/portfolio.js
@@ -25,8 +25,7 @@ const greeting = {
   subTitle: emoji(
     "A passionate Full Stack Software Developer 🚀 having an experience of building Web applications with Java / Spring / SpringBoot /Angular and some other cool libraries and frameworks."
   ),
-  resumeLink:
-    "https://drive.google.com/file/d/11AgjSAqMmQR22lnmu4JVpN6EVGM_PWaF/view?usp=sharing", // Set to empty to hide the button
+  resumeLink:"https://drive.google.com/file/d/11AgjSAqMmQR22lnmu4JVpN6EVGM_PWaF/view?usp=sharing", // Set to empty to hide the button
   displayGreeting: true // Set false to hide this section, defaults to true
 };
 
